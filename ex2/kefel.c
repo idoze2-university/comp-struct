736cd4852aca67b299d4e44aab35c4ceb0dc19e0
idoz <idoz@idoz7490-ubuntu14.checkpoint.com>
2019-12-15 19:02:48 +0200

    updating work Dec 2019

--- ex2/kefel.c
+++ ex2/kefel.c
@@ -1,93 +1,125 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define HEADER ".section .text\n.globl kefel\nkefel: mov %%edi,%%eax\n"
-#define FOOTER "ret\n"
-#define RET_0 "mov $0,%%eax\n"
+#define HEADER ".section .text\n.globl kefel\nkefel: "
+#define MOV_SELF "movl %%edi,%%eax\n"
+#define RET_0 "movl $0,%%eax\n"
 #define RET_MINUS "neg %%eax\n"
-#define LEA_N "lea (%%eax,%%eax,%d),%%eax\n"
+#define LEA_N "leal (%%eax,%%eax,%d),%%eax\n"
 #define SHL_N "shl $%d,%%eax\n"
-#define SEVEN_EAX "sal $3,%%eax\nsub %%edi,%%eax\n"
-#define ADD_SELF "add %%edi,%%eax\n"
-#define SUB_SELF "sub %%edi,%%eax\n"
+#define MOV_SHL_NC "movl %%edi,%%ecx\nshl $%d,%%ecx\n"
+#define SUB_SELF "subl %%edi,%%eax\n"
+#define ADD_SELF "addl %%edi,%%eax\n"
+#define SELF_ADD_SUB_NEG !negative ? ADD_SELF : SUB_SELF
+#define SELF_ADD_SUB_NEG_INVERT negative ? ADD_SELF : SUB_SELF
+#define SUB_EAXC "subl %%ecx,%%eax\n"
+#define ADD_EAXC "addl %%ecx,%%eax\n"
+#define EAXC_ADD_SUB_NEG !negative ? ADD_EAXC : SUB_EAXC
+#define EAXC_ADD_SUB_NEG_INVERT negative ? ADD_EAXC : SUB_EAXC
+#define FOOTER "ret\n"
 
-void create_asm(int);
-void print_op(FILE *, int);
-
-int isPowerOf2(int i)
+void create2(FILE *fp, int k)
 {
-    if (i == 1)
-        return 1;
-    if (i % 2)
-        return 0;
-    return isPowerOf2(i >> 1);
+    fprintf(fp, HEADER);
+    int negative = (k < 0);
+    !k ? fprintf(fp, RET_0) : 0;
+    k && (k != -1) ? fprintf(fp, MOV_SELF) : 0;
+    if (negative)
+        k *= -1;
+    if (k > 1)
+    {
+        int factors[] = {9, 5, 3};
+        int f = 0;
+        int count = 0;
+        for (; f < 3; f++)
+        {
+            count = 0;
+            if (!(k % factors[f]))
+            {
+                int factor = factors[f];
+                int temp = k;
+                for (; !(temp % 2); temp >>= 1, count++)
+                    ;
+                if (temp == factor)
+                {
+                    fprintf(fp, LEA_N, factor - 1);
+                    count > 0 ? fprintf(fp, SHL_N, count) : 0;
+                    negative ? fprintf(fp, RET_MINUS) : 0;
+                    break;
+                }
+            }
+        }
+        int addone = 0;
+        if (f == 3) //for ended without printing anything, assuming k is not in the form f*2^n (f in [9,5,3]).
+        {
+            int first = 1;
+            while (k)
+            {
+                int mask = 1;
+                int m, n;
+                for (m = 0; (k ^ mask) > k; mask <<= 1, m++)
+                    ;
+                for (n = m - 1; (k ^ mask) < k; mask <<= 1, n++)
+                    ;
+                if (negative)
+                {
+                    int temp = n;
+                    n = m;
+                    m = temp;
+                }
+                if (!n)
+                {
+                    addone = 1;
+                    k--;
+                }
+                else
+                {
+                    if (first)
+                    {
+                        fprintf(fp, SHL_N, negative || !(n - m) ? n : n + 1);
+                        (negative && n == m) ? fprintf(fp, RET_MINUS) : 0;
+                    }
+                    else
+                    {
+                        fprintf(fp, MOV_SHL_NC, negative || !(n - m) ? n : n + 1);
+                        fprintf(fp, negative && n == m ? SUB_EAXC : ADD_EAXC);
+                    }
+                    if (n - m)
+                    {
+                        if (m > 0)
+                        {
+                            fprintf(fp, MOV_SHL_NC, negative ? m + 1 : m);
+                            fprintf(fp, SUB_EAXC);
+                        }
+                        else
+                        {
+                            fprintf(fp, SUB_SELF);
+                        }
+                    }
+                    k += !negative ? (1 << m) - (1 << n + 1) : (1 << n) - (1 << m + 1);
+                    first = 0;
+                }
+            }
+        }
+        addone ? fprintf(fp, SELF_ADD_SUB_NEG) : 0;
+        fprintf(fp, FOOTER);
+    }
+    else
+    {
+        negative ? fprintf(fp, RET_MINUS) : 0;
+        fprintf(fp, FOOTER);
+    }
+    fclose(fp);
 }
+
 int main(int argc, char *argv[])
 {
-    int k;
     if (argc <= 1)
     {
         printf("Please provide input for k.\n");
         return 1;
     }
-    create_asm(atoi(argv[1]));
+    FILE *fp = fopen("kefel.s", "w");
+    create2(fp, atoi(argv[1]));
     return 0;
-}
-
-void create_asm(int k)
-{
-    FILE *fp;
-    fp = fopen("kefel.s", "w");
-    fprintf(fp, HEADER);
-    print_op(fp, k);
-    fprintf(fp, FOOTER);
-    fclose(fp);
-}
-
-void print_op(FILE *fp, int i)
-{
-    int count = 0;
-    switch (i)
-    {
-    case 1:
-        break;
-    case 0:
-        fprintf(fp, RET_0);
-        break;
-    case -1:
-        fprintf(fp, RET_MINUS);
-        break;
-    case 3:
-    case 5:
-    case 9:
-        fprintf(fp, LEA_N, i - 1);
-        break;
-    case 7:
-        fprintf(fp, SEVEN_EAX);
-        break;
-    default:
-        if (i % 2)
-        {
-            if (isPowerOf2(i + 1))
-            {
-                print_op(fp, i + 1);
-                fprintf(fp, SUB_SELF);
-            }
-            else
-            {
-                print_op(fp, i - 1);
-                fprintf(fp, ADD_SELF);
-            }
-        }
-        else
-        {
-            for (; i % 2 == 0; i >>= 1, count++)
-            {
-            }
-            printf("%d\n", i);
-            print_op(fp, i);
-            count > 0 ? fprintf(fp, SHL_N, count) : 0;
-        }
-        break;
-    }
 }
--- ex2/kefel.s
+++ ex2/kefel.s
@@ -1,9 +1,26 @@
 .section .text
 .globl kefel
-kefel: mov %edi,%eax
-lea (%eax,%eax,2),%eax
-shl $3,%eax
-add %edi,%eax
+kefel: movl %edi,%eax
 shl $1,%eax
-add %edi,%eax
+movl %edi,%ecx
+shl $7,%ecx
+addl %ecx,%eax
+movl %edi,%ecx
+shl $3,%ecx
+subl %ecx,%eax
+movl %edi,%ecx
+shl $8,%ecx
+addl %ecx,%eax
+movl %edi,%ecx
+shl $10,%ecx
+addl %ecx,%eax
+movl %edi,%ecx
+shl $12,%ecx
+addl %ecx,%eax
+movl %edi,%ecx
+shl $18,%ecx
+addl %ecx,%eax
+movl %edi,%ecx
+shl $15,%ecx
+subl %ecx,%eax
 ret
